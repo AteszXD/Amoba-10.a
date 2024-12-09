485aba1cc11a01b47c7c5f01676c1ebd5d0a51f8
AteszXD <144881460+AteszXD@users.noreply.github.com>
2024-12-09 17:28:27 +0100

    2024. 12. 06
    Ez péntek délutáni verzió
    
    Beraktam egy új, indexelt táblázatot (ezekben még nincs rács), és kidolgoztam a jel rakást is, tehát már egy nagyon alap játszható

--- amobaprojekt.py
+++ amobaprojekt.py
@@ -1,4 +1,6 @@
 # 10x10 táblázat
+
+# Megoldás 1, ez kirajzolja, bár indexezni nem tudom hogy fogjuk
 """
 def Jatekter():
     print("┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐")
@@ -11,15 +13,42 @@ def Jatekter():
 Jatekter()
 """
 
+# Megoldás 2, indexelt, de nem rácsos, Amíg nincs rács addig tesztnek az E betű lesz (E = Empty)
+
+tablazat = [
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E']
+]
+
 # Milyen jelet rakjon
-jel = "x"
+jel = "X"
 lepesszam = 0
-for i in range(5):
-    input("Hova rak? ")
-    if lepesszam % 2 == 0:
-        jel = "x"
+
+# Hova tegye a jelet
+while True: # Itt majd lesz konkrét ellenőrzés
+    sor = int(input("Hányadik sorba szeretne tenni? (1-10) ")) 
+    oszlop = int(input("Hányadik oszloba szeretne tenni? (1-10) "))
+    while not(sor > 0 and sor < 11) or not(oszlop > 0 and oszlop < 11):
+        print("Valamelyik pozíció a táblázaton kívül esik!")
+        sor = int(input("Hányadik sorba szeretne tenni? (1-10) ")) 
+        oszlop = int(input("Hányadik oszloba szeretne tenni? (1-10) "))
+    while (tablazat[sor-1][oszlop-1]) != 'E':
+        print("Ez a hely már foglalt!")
+        sor = int(input("Hányadik sorba szeretne tenni? (1-10) ")) 
+        oszlop = int(input("Hányadik oszloba szeretne tenni? (1-10) ")) 
+    (tablazat[sor-1][oszlop-1]) = jel
+    for sor in tablazat:
+        print(' '.join(sor))
+    if lepesszam % 2 == 1:
+        jel = "X"
     else:
-        jel = "o"
-    print(jel)
+        jel = "O"
     lepesszam += 1
-    
